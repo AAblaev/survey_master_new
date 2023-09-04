aba7ae5f40e236c61979b7823988afc15fe71049
Alex Kim <kim_alex@mail.ru>
2023-09-04 13:28:52 +0300

    fixed url

--- src/services/api/const.ts
+++ src/services/api/const.ts
@@ -4,11 +4,11 @@ export const PATH_NAME_FOR_DEV = "http://192.168.0.133:5004/api/survey/";
 export const PATH_NAME_FOR_DEV_II = "http://192.168.0.133:5004/api/survey/";
 
 export const PATH_NAME = __IS_PROD_BUNDLE_MODE__
-  ? document.location.origin + document.location.pathname
+  ? document.location.origin + document.location.pathname + "api/survey/"
   : PATH_NAME_FOR_DEV;
 
 export const PATH_NAME_II = __IS_PROD_BUNDLE_MODE__
-  ? document.location.origin + document.location.pathname
+  ? document.location.origin + document.location.pathname + "api/survey/"
   : PATH_NAME_FOR_DEV_II;
 
 export const DEFAULT_SURVEY_ID = "1";
