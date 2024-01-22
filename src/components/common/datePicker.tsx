fd757b99e27c26b695ffc3e3e026ca9122d15615
Alex Kim <kim_alex@mail.ru>
2024-01-22 11:33:59 +0300

    in-progress

--- src/components/common/datePicker.tsx
+++ src/components/common/datePicker.tsx
@@ -4,18 +4,30 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs, { Dayjs } from "dayjs";
 import "dayjs/locale/ru";
+import { ruRU } from "@mui/x-date-pickers/locales";
+
+const ruLocale =
+  ruRU.components.MuiLocalizationProvider.defaultProps.localeText;
 
 const MyDatePicker = () => {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs("2022-04-17"));
 
   console.log("value", value);
+  console.log("test", dayjs("22.02.2001"));
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+    <LocalizationProvider
+      dateAdapter={AdapterDayjs}
+      adapterLocale="ru"
+      localeText={ruLocale}
+    >
       <MuiDatePicker
         views={["year", "month", "day"]}
         value={value}
-        onChange={(newValue) => {
-          console.log("onChange", newValue);
+        onChange={(newValue, context) => {
+          console.log("newValue", newValue);
+          console.log("context", context);
+
           setValue(newValue);
         }}
         dayOfWeekFormatter={(_day, weekday) => `${weekday.format("dd")}`}
